0b1fd9ed1a98096db3c20bd4e2f95cc3bfb8020a
sean <sean@ubuntu-22.04.02-LTS.myguest.virtualbox.org>
2023-04-22 23:04:18 +0300

    Save

--- assignment2.py
+++ assignment2.py
@@ -22,6 +22,81 @@ class Assignment2(object):
         else:
             p = 0.1
         return [p, 1-p]
+    
+    def interval_lists_intersection(self, intervals1, intervals2):
+        """
+        Input: two lists of intervals.
+
+        Returns: the intervals of the intersection between the two lists.
+        """
+        intersection = []
+        index1 = 0
+        index2 = 0
+
+        while index1 < len(intervals1) and index2 < len(intervals2):
+            # Checking if intervals1[index1] intersects intervals2[index2]
+            start = max(intervals1[index1][0], intervals2[index2][0])
+            end = min(intervals1[index1][1], intervals2[index2][1])
+            if start <= end:
+                intersection.append([start, end])
+
+            # Skip the interval with the lowest endpoint
+            if intervals1[index1][1] < intervals2[index2][1]:
+                index1 += 1
+            else:
+                index2 += 1
+
+        return intersection
+
+    # DELETE THIS
+    def calculate_len_intersection(self, list1, list2):
+        """Calculate the length of the intersection between the intervals in list1 and list2.
+        Input: list1 - a list of tuples, every tuple is an interval.
+            list2 - a list of tuples, every tuple is an interval.
+
+        Returns: The length of the intersection between the intervals in list1 and list2.
+        """
+        len_intersection = 0
+        pointer_list1 = 0
+        pointer_list2 = 0
+        while pointer_list1 < len(list1) and pointer_list2 < len(list2):
+            start = max(list1[pointer_list1][0], list2[pointer_list2][0])
+            end = min(list1[pointer_list1][1], list2[pointer_list2][1])
+            if start < end:
+                len_intersection += (end - start)
+            if list1[pointer_list1][1] == list2[pointer_list2][1]:
+                pointer_list1 += 1
+                pointer_list2 += 1
+            elif list1[pointer_list1][1] < list2[pointer_list2][1]:
+                pointer_list1 += 1
+            else:
+                pointer_list2 += 1
+        return len_intersection
+    # DELETE THIS
+
+    def true_error_for_intervals(self, intervals):
+        """
+        Returns the true error with resepect to the distribution P and the passed intervals (define hypothesis).
+        """
+        # accumulates area where passed intervals are in [0,0.2]U[0.4,0.6]U[0.8,1]
+        hypothesis_1_high_prob_area = 0
+        # accumulates area where passed intervals are in [0.2,0.4]U[0.6,0.8]
+        hypothesis_1_low_prob_area = 0
+        intersection_1_high_prob = self.interval_lists_intersection([[0,0.2], [0.4,0.6], [0.8,1]], intervals)
+        intersection_1_low_prob = self.interval_lists_intersection([[0.2,0.4], [0.6,0.8]], intervals)
+        for interval in intersection_1_high_prob:
+            hypothesis_1_high_prob_area += (interval[1] - interval[0])
+        for interval in intersection_1_low_prob:
+            hypothesis_1_low_prob_area += (interval[1] - interval[0])
+        
+        hypothesis_0_high_prob_area = 0.4 - hypothesis_1_low_prob_area
+        hypothesis_0_low_prob_area = 0.6 - hypothesis_1_high_prob_area
+
+        # real gives 1 while hypothesis 0
+        expect_real_1_hypo_0 = (0.8 * hypothesis_0_low_prob_area) + (0.1 * hypothesis_0_high_prob_area)
+        expect_real_0_hypo_1 = (0.2 * hypothesis_1_high_prob_area) + (0.9 * hypothesis_1_low_prob_area)
+        return expect_real_1_hypo_0 + expect_real_0_hypo_1
+
     # HELP FUNCTIONS MOVE
 
     def sample_from_D(self, m):
@@ -57,8 +132,42 @@ class Assignment2(object):
             A two dimensional array that contains the average empirical error
             and the average true error for each m in the range accordingly.
         """
-        # TODO: Implement the loop
-        pass
+        ret_val = []
+        empirical_err_averages = []
+        true_err_averages = []
+        sample_sizes = list(range(m_first, m_last + 1, step))
+        for n in sample_sizes:
+            empirical_err_sum = 0
+            true_err_sum = 0
+            for experiment in range(T):
+                samples = self.sample_from_D(n)
+                samples_x = [sample[0] for sample in samples]
+                samples_y = [sample[1] for sample in samples]
+                erm = intervals.find_best_interval(samples_x, samples_y, k)
+                erm_intervals, error_count = erm
+                empirical_err = error_count / n
+                true_err = self.true_error_for_intervals(erm_intervals)
+
+                empirical_err_sum += empirical_err
+                true_err_sum += true_err
+            
+            empirical_err_avg = empirical_err_sum / T
+            true_err_avg = true_err_sum / T
+            ret_val.append([empirical_err_avg, true_err_avg])
+            empirical_err_averages.append(empirical_err_avg)
+            true_err_averages.append(true_err_avg)
+        
+        plt.figure(1)
+        plt.title("Empirical and true errors averaged as a function of n (sample size)")
+        plt.xlabel("n (Sample size)")
+        plt.ylabel("Error")
+        plt.plot(sample_sizes, empirical_err_averages, '.', label="empirical error")
+        plt.plot(sample_sizes, true_err_averages, '.', label="true error")
+        plt.legend()
+
+        return np.array(ret_val)
+
+
 
     def experiment_k_range_erm(self, m, k_first, k_last, step):
         """Finds the best hypothesis for k= 1,2,...,10.
@@ -70,9 +179,35 @@ class Assignment2(object):
 
         Returns: The best k value (an integer) according to the ERM algorithm.
         """
-        # TODO: Implement the loop
-        pass
+        empirical_errors = []
+        true_errors = []
+        k_vals = list(range(k_first, k_last + 1, step))
+        k_star = -1
+        k_star_err = 10
+        samples = self.sample_from_D(m)
+        samples_x = [sample[0] for sample in samples]
+        samples_y = [sample[1] for sample in samples]
+        for k in k_vals:
+            erm_intervals, error_count = intervals.find_best_interval(samples_x, samples_y, k)
+            empirical_error = error_count / m
+            true_error = self.true_error_for_intervals(erm_intervals)
+            empirical_errors.append(empirical_error)
+            true_errors.append(true_error)
+            if k_star_err > empirical_error:
+                k_star = k
+                k_star_err = empirical_error
+        
+        plt.figure(2)
+        plt.title("Empirical and true errors as a function of k")
+        plt.xlabel("k")
+        plt.ylabel("Error")
+        plt.plot(k_vals, empirical_errors, '.', label="empirical error")
+        plt.plot(k_vals, true_errors, '.', label="true error")
+        plt.legend()
 
+        plt.show()
+
+        return k_star
 
     def cross_validation(self, m):
         """Finds a k that gives a good test error.
@@ -80,8 +215,10 @@ class Assignment2(object):
 
         Returns: The best k value (an integer) found by the cross validation algorithm.
         """
-        # TODO: Implement me
-        pass
+        samples = self.sample_from_D(m)
+        samples_x = [sample[0] for sample in samples]
+        samples_y = [sample[1] for sample in samples]
+        
 
     #################################
     # Place for additional methods
@@ -92,7 +229,7 @@ class Assignment2(object):
 
 if __name__ == '__main__':
     ass = Assignment2()
-    ass.experiment_m_range_erm(10, 100, 5, 3, 100)
+    # ass.experiment_m_range_erm(10, 100, 5, 3, 100)
     ass.experiment_k_range_erm(1500, 1, 10, 1)
     ass.cross_validation(1500)
 
